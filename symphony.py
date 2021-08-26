f6206b2fca84af5baeba18c2ea65fe76c3b7aa0e
Ritobhash <ritobhash@gmail.com>
2021-08-26 13:53:17 +0530

    playlist support added

--- symphony.py
+++ symphony.py
@@ -5,17 +5,41 @@ import constants
 
 class Symphony:
     def __init__(self):
-        ydlOpts = {'format': 'bestaudio/best'}
+        ydlOpts = {'format': 'bestaudio'}
         self.ytdl = youtube_dl.YoutubeDL(ydlOpts)
 
-    def audioStreamByURL(self, url: str) -> str:
-        # Returns audio stream url
+        self.queue = []
+
+    def processQueue(self, url):
         videoInfo = self.ytdl.extract_info(url, download=False)
-        streamURL = videoInfo['formats'][0]['urls']
-        return streamURL
+
+        if '_type' in videoInfo:
+            for entry in videoInfo['entries']:
+                self.__processVideo(entry)
+        else:
+            self.__processVideo(videoInfo)
+
+        return self.queue
+
+    def __processVideo(self, videoDict: dict) -> None:
+        title = videoDict['title']
+        pageURL = videoDict['webpage_url']
+        streamURL = videoDict['formats'][0]['url']
+        durationInSec = videoDict['duration']
+        thumbnailURL = videoDict['thumbnail']
+
+        videoInfo = {
+            'title': title,
+            'pageURL': pageURL,
+            'streamURL': streamURL,
+            'durationInSec': durationInSec,
+            'thumbnailURL': thumbnailURL
+        }
+
+        self.queue.append(videoInfo)
 
 
 if __name__ == "__main__":
     symphony = Symphony()
-    info = symphony.audioStreamByURL(constants.TEST_YT_URL)
+    info = symphony.processQueue(constants.TEST_YT_URL)
     print(info)
